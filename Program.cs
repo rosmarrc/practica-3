7b4440f72719bfe112b7d2592026a527df482ecf
rosmarrc <79125757+rosmarrc@users.noreply.github.com>
2021-03-10 18:26:58 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pract_3_eje_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Abecedario al reves:");
+
+            char letra= 'Z';
+            letra++;
+
+            do
+            {
+                letra--;
+                Console.WriteLine(letra);
+            } while (letra > 'A');
+
+          
+            Console.ReadKey();
+        }
+    }
+}
