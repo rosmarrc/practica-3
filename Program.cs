5c2148f02fe593fb614bed9335ca35cf8d3bf199
rosmarrc <79125757+rosmarrc@users.noreply.github.com>
2021-03-10 18:21:34 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pract_3_eje_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            Console.WriteLine("NUMEROS DEL 1 AL 10");
+            do
+            {
+                x++;
+                Console.WriteLine(x);
+            } while (x < 10);
+
+            Console.ReadKey();
+        }
+    }
+}
