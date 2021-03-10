b435c6e8b67231c1e7429db16e302e357471020a
rosmarrc <79125757+rosmarrc@users.noreply.github.com>
2021-03-10 18:20:32 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pract_3_eje_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10 * 10;
+            int y = 0;
+            Console.WriteLine("Producto de 10x10: " + x + "\n");
+            Console.WriteLine("LOS 10 PRIMEROS NUMEROS PARES A PARTIR DEL PRODUCTO DE 10X10:" );
+            do
+            {
+                x = x + 2;
+                y++;
+                Console.WriteLine(x);
+            } while (y < 10);
+
+            Console.ReadKey();
+            
+        
+        }
+    }
+}
